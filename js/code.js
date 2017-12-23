cd7d0097b6dfab832e0cc1dacf267b7e92ae34e3
francescadelucca <francesca.delucca@libero.it>
2017-12-23 15:17:48 +0100

    Update code.js
    https

--- js/code.js
+++ js/code.js
@@ -19,8 +19,8 @@ function initmap() {
 	map = new L.Map('map');
 
 	// create the tile layer with correct attribution
-	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
 	var osm = new L.TileLayer(osmUrl, {minZoom: 9, maxZoom: 19, attribution: osmAttrib});		
 
 	// start the map in South-East England
